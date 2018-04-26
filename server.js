1a435472fb4c20debb59004abb96a3ccd81dadde
mnap <learn@10g.pl>
2018-04-26 22:36:48 +0200

    Add delete request on del_user address

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*eslint-disable no-console*/
 var express = require('express');
 var app = express();
 
@@ -9,6 +10,11 @@ app.get('/', function(req, res) {
 app.post('/', function(req, res) {
     console.log('Received POST request on main site');
     res.send('Hello POST!');
+});
+
+app.delete('/del_user', function(req, res) {
+    console.log('Received DELETE request on del_user site');
+    res.send('Hello DELETE!');
 });
 
 var server = app.listen(3000, function() {

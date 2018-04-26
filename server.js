99fcee15f1c6f8e63016e4580868f0d5c1d3ad6d
mnap <learn@10g.pl>
2018-04-26 22:31:22 +0200

    Add post request

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ app.get('/', function(req, res) {
     res.send('Hello GET!');
 });
 
+app.post('/', function(req, res) {
+    console.log('Received POST request on main site');
+    res.send('Hello POST!');
+});
+
 var server = app.listen(3000, function() {
     console.log('Example app is listening on localhost port 3000');
 });

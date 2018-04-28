a84147bddfc5408d6724980bdd02c82f9e2d5775
mnap <learn@10g.pl>
2018-04-28 11:54:08 +0200

    Add userform endpoint

--- server.js
+++ server.js
@@ -6,7 +6,15 @@ app.use(express.static('assets'));
 
 app.get('/', function(req, res) {
     console.log('Received GET request on main site');
-    res.send('Hello World!');
+    res.sendFile('/index.html');
+});
+
+app.get('/userform', function(req, res) {
+    const response = {
+        first_name: req.query.first_name,
+        last_name: req.query.last_name
+    };
+    res.end(JSON.stringify(response));
 });
 
 var server = app.listen(3000, 'localhost', function() {

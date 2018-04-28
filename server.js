ae27630eee5523449806974e424ab16e9bd5d54a
mnap <learn@10g.pl>
2018-04-28 11:19:53 +0200

    Update server script

--- server.js
+++ server.js
@@ -2,33 +2,13 @@
 var express = require('express');
 var app = express();
 
-app.get('/:id', function(req, res) {
+app.get('/', function(req, res) {
     console.log('Received GET request on main site');
-    res.send('Add ' + req.params.id + ' identifier');
+    res.send('Hello World!');
 });
 
-app.post('/', function(req, res) {
-    console.log('Received POST request on main site');
-    res.send('Hello POST!');
-});
-
-app.delete('/del_user', function(req, res) {
-    console.log('Received DELETE request on del_user site');
-    res.send('Hello DELETE!');
-});
-
-app.get('/list_user', function(req, res) {
-    console.log('Received GET request on list_user site');
-    res.send('Users list');
-});
-
-app.get('/ab*cd', function(req, res) {
-    console.log('Received GET request on ab*cd site');
-    res.send('Pattern found');
-});
-
-app.listen(3000);
-
-app.use(function (req, res, next) {
-    res.status(404).send('Sorry, cannot find requested site');
+var server = app.listen(3000, 'localhost', function() {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Example app listening on http://' + host + ':' + port);
 });

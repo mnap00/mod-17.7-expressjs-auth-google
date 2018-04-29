815d5ff5a1d4f49ef2bc1e71abffe5e29d54ce27
mnap <learn@10g.pl>
2018-04-29 11:41:35 +0200

    Update project meta; clean up structure and scripts

--- server.js
+++ server.js
@@ -7,32 +7,6 @@ app.set('views', './views');
 
 app.use(express.static('assets'));
 
-app.use('/store', function(req, res, next) {
-    console.log('I\'m the middleware for \'/store\' request');
-    next();
-});
-
-app.get('/', function(req, res) {
-    console.log('Received GET request on main site');
-    res.sendFile('/index.html');
-});
-
-app.get('/userform', function(req, res) {
-    const response = {
-        first_name: req.query.first_name,
-        last_name: req.query.last_name
-    };
-    res.end(JSON.stringify(response));
-});
-
-app.get('/store', function(req, res) {
-    res.send('This is store');
-});
-
-app.get('/first-template', function(req, res) {
-    res.render('first-template');
-});
-
 app.get('/welcome', function(req, res) {
     res.render('aa-welcome');
 });

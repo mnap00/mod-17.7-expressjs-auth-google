f5f584ba595b314f429041086de3580957ba9cd8
mnap <learn@10g.pl>
2018-04-29 11:49:55 +0200

    Add configuration file

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '747281243901-u6f3iqvr9q77na2eobt9n5al7p7s2r3o.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': '6op_2FCWcgee4i4ryxswMzvI',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
